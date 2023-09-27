73ca98f3bfa5faeb6e3756c391601337f33c366e
Luna Huynh <144747523+kphuynh118@users.noreply.github.com>
2023-09-26 20:48:48 -0700

    Update insert a matrix.c (addition & multiplication)

--- insert a matrix.c
+++ insert a matrix.c
@@ -1,9 +1,56 @@
 // Add shared functions to this file.
 
-int main(){
+#include <stdio.h>
+
+void display_matrix(int nRows, int nCols, int matrix[nRows][nCols]){
+    int row; int column; 
+    for(row=0; row < nRows; ++row){
+        for(column=0; column < nCols; ++column)
+            printf("%5i ",matrix[row][column]); 
+        printf("\n");
+    }
+} 
+
+void matrix_addition(int nRows, int nCols, int A[nRows][nCols], int B[nRows][nCols]){
+     int row; int column; 
+    int C[nRows][nCols];
+
+    for(row=0; row < nRows; ++row){
+        for(column=0; column < nCols; ++column){
+            C[row][column] = A[row][column] + B[row][column]; 
+        }
+    }
+    printf("The Addition of two matrix: \n"); 
+    display_matrix(nRows, nCols,C);
+}
+
+void matrix_multiplication(int a, int b, int matrix1[][100], int m, int n, int matrix2[][100]){
+    int row; int column; int k;
+    int C[a][n];
+    if(b != m) //number of columns of matrix1 is not the same as number of rows of matrix2
+        printf("Can not do the multiplication. \n"); 
+    else
+        for(row = 0; row < a; row++){
+            for(column = 0; column < n; column++){
+                C[row][column]=0;
+            }
+        }
+
+        for(row = 0; row < a; row++){
+            for(column = 0; column < n; column++){
+                for(k = 0; k < m; k++){
+                    C[row][column] += matrix1[row][k]*matrix2[k][column];
+                }
+            }
+        }
+        printf("The Product of two matrix: \n"); 
+        display_matrix(a,n,C); 
+}
+
+int main() {
     int row; int column; int n; 
-    float A[100][100];
-    float b[100];
+    int A[100][100];
+    int b[100][100];
 
     printf("Enter the order of your matrix A: ");
     scanf("%d",&n);
@@ -12,19 +59,17 @@ int main(){
     for (row=0; row < n; ++row){
         for (column=0; column < n; ++column){
             printf("A[%d][%d]= " ,row,column); 
-            scanf("%f", &A[row][column]); 
-            }
+            scanf("%d", &A[row][column]); 
+            }  
         }
-        
-            
+
     printf("Enter the elements of your b vector: \n");
     for (row=0; row < n; ++row){
-        printf("b[%d]= ",row);
-        scanf("%f", &b[row]);
+        for(column=0; column < 1; ++column){
+            printf("b[%d]= ",row);
+            scanf("%d", &b[row][column]);
+        }  
     }
-    return 0;
-        
-}
-    cross_product(A,b);
-    return 0;
+    matrix_multiplication(n,n,A,n,1,b);
+    return 0;       
 }
