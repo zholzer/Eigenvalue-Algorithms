a6458b5d0fb9141fdf0e7a43b0cc3c0bfa189eba
Luna Huynh <144747523+kphuynh118@users.noreply.github.com>
2023-09-26 18:44:01 -0700

    Update and rename EAFunctions.c to insert_matrix.c

--- insert a matrix.c
+++ insert a matrix.c
@@ -1,11 +1,4 @@
 // Add shared functions to this file.
-void cross_product(float vector_A[10][10], float vector_b[10]){
-    float P[10];
-    P[0] = vector_A[0][0]*vector_b[0] + vector_A[0][1]*vector_b[1];
-    P[1] = vector_A[1][0]*vector_b[0] + vector_A[1][1]*vector_b[1];
-    float Ab[] = {P[1],P[0]};
-    printf("A*b = %f %f \n", Ab[0],Ab[1]);
-}
 
 int main(){
     int row; int column; int n; 
