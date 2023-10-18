015eea1126f5d49330fe58b54c4be7c2ddbcc8a1
zholzer <144373470+zholzer@users.noreply.github.com>
2023-10-17 23:53:07 -0700

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include "functions.h"
-// function declarations
 
 int main(void){
     printf("Welcome to the Power Iteration program.\n");
@@ -13,9 +12,11 @@ int main(void){
     printf("Please choose which test file to run by entering an integer between 1 and 5.\n");
     int fileNum;
     char fileName[15];
+    // gets case number from user, currently not in any particular order
     scanf("%d", &fileNum);
     switch (fileNum){
     case 1:
+    // test file one breaks in Lanczos ******
         strcpy(fileName, "testCase1.txt");
         printf("You chose test file one with a 10 x 10 matrix.\n");
         break;
@@ -27,7 +28,7 @@ int main(void){
 
     case 3:
         strcpy(fileName, "testCase3.txt");
-        printf("You chose test file three with a 10 x 10 real matrix.\n");
+        printf("You chose test file three with a 5 x 5 real matrix.\n");
         break;
 
     case 4:
@@ -45,25 +46,32 @@ int main(void){
         break;
     }
 
-    // Opens and reads indicated test file to store the values in matrix A
+    // Get size of test file matrix. 
     int i, j;
     double c, n;
     int count, dim;
-    //char contents[500];
-    // initialize matrix
+    // Opens file
     FILE *testFile;
     testFile = fopen(fileName, "r");
+    // increases count for each long float in test file
     while (1){
         fscanf(testFile, "%lf", &c);
         if(feof(testFile)){break;}
         count++;
     }
     
+    // file consists of two equally sized n by n matrices; 
+    // the first matrix is read in as the real values (a of a+bi)
+    // the second is read in as the imaginary values (b of a+bi)
+    // the size of the matrix is the # digits / 2 square rooted since n by n
     dim = sqrt(count/2);
     //double complex A[dim][dim];
-    double complex (*A)[dim] = malloc(sizeof(double complex[dim][dim]));
-
+    //double complex (*A)[dim] = malloc(sizeof(double complex[dim][dim]));
+    double complex (*A)[dim];
+    A = calloc(dim, sizeof(*A));
+    // rewind file pointers to store matrix values
     rewind(testFile);
+    // real matrix
     for (i=0; i<dim; i++){
         for (j=0; j<dim; j++){
             fscanf(testFile, "%lf", &n);
@@ -71,6 +79,7 @@ int main(void){
             if(feof(testFile)){break;}
         }
     }
+    // imaginary matrix
     for (i=0; i<dim; i++){
         for (j=0; j<dim; j++){
             fscanf(testFile, "%lf", &n);
@@ -78,30 +87,36 @@ int main(void){
             if(feof(testFile)){break;}
         }
     }
+    // done with the file
     fclose(testFile);
     
     printf("This test file contains the matrix: \n");
     displayMatrix(dim, dim, A);
 
+    // allows user to input how many iterations they want Lanczos to run
     int iter;
     int booL = 0;
     while(booL == 0){
-        printf("Input number of iterations as an integer (must be less then %d).\n", dim);
+        printf("Input number of iterations of the Lanczos Algorithm as an integer (must be less then %d).\n", dim);
         scanf("%d", &iter);
         if (iter > dim){
             printf("Must be less then %d. Retry. \n", dim);
         }
         else{booL = 1;}
     }
+
+    // calls algorithms
     //printf("The Power Iteration produces ");
     //power_iteration(dim, A); // runs with 10 by 10
-    //printf("The Power Iteration produces ");
+    //printf("The Rayleigh Iteration produces ");
     //rayleighIteration(dim, A); // very slow with 10 by 10
     printf("The Lanczos Algorithm produces ");
     LanczosAlgorithm(dim, A, iter); // segfault 10 by 10
-    //free(A);
+    // free pointer
+    free(A);
     return 0;
 }
 
+// references
 // https://stackoverflow.com/questions/36890624/malloc-a-2d-array-in-c
-// https://stackoverflow.com/questions/36890624/malloc-a-2d-array-in-c
+//stackoverflow.com/questions/29977084/calloc-a-two-dimensional-array
